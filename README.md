e96fbfd38c6561411292ae6471cd09c142e93f3c
kkabetani <k.kazhr@gmail.com>
2013-09-08 23:24:30 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+login template
+
+ruby 2.0.0
+rails 4.0.0
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LoginTemplate::Application.config.secret_key_base = '59adc186155db033c619495e0e32bd8cf9468f0af9fafe262295db9c3e5def415093a98a32672983aaebcba1b3063637551f105f9184ea69992115162fd856ff'
